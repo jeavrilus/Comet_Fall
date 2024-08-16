506e4368775793d4329b99ffce1eb88f5c394c91
Jean <jeavrilus@gmail.com>
2024-08-16 13:03:56 -0400

    Vérifier si la comète a atteint le sol, si oui on la retire du jeu

--- comet.py
+++ comet.py
@@ -3,7 +3,8 @@ import random
 
 # DEFINITION DE LA CLASSE COMET (Cette fameuse boule de feu céleste)
 class Comet(pygame.sprite.Sprite):
-    def __init__(self) -> None:
+
+    def __init__(self, comet_event) -> None:
         super().__init__()
         # définir l'image associé à cette comète
         self.image = pygame.image.load("assets/comet.png")
@@ -11,6 +12,16 @@ class Comet(pygame.sprite.Sprite):
         self.speed = random.randint(1, 3)
         self.rect.x = random.randint(20, 700)
         self.rect.y = - random.randint(0, 700)
+        self.comet_event = comet_event
+
+    def remove(self):
+        self.comet_event.all_comets.remove(self)
         
     def fall(self):
         self.rect.y += self.speed
+
+        # ne tombe pas sur le sol
+        if self.rect.y >= 500:
+            print("sol")
+            # retirer la comète du jeu
+            self.remove()
