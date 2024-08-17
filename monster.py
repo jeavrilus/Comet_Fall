c490e62bd2ec4a374d5879d3ab0c353fd9beae65
Jean <jeavrilus@gmail.com>
2024-08-17 14:56:43 -0400

    feat: Vérifier si la barre d'événement est chargée à son maximum pour supprimer les monstres de la fenêtre du jeu et faire appel à la méthode qui permet de déclencher la pluie des comètes

--- monster.py
+++ monster.py
@@ -21,28 +21,31 @@ class Monster(pygame.sprite.Sprite):
         # infliger les degats
         self.health -= amount
         # verifier si son nouveau nombre de points de vie est inferieur a 0
-        if self.health == 0:
+        if self.health <= 0:
             # Reapparaitre comme un nouveau montre avec des caracteristiques differentes en x, health et en speed
             self.rect.x = 1000 + random.randint(0, 280)
             self.health = self.max_health
             self.speed = random.randint(1, 2)
 
-    
+            # si la barre d'événement est chargée à son maximum
+            if self.jeu.comet_event.is_full_loaded():
+                # retirer les monstres du jeu
+                self.jeu.all_monsters.remove(self)
 
-    # Creer un jauge de vie
+                # appel de la méthode pour essayer de déclencher la pluie des comètes
+                self.jeu.comet_event.attempt_fall()
+
+    # Créer une jauge de vie
     def update_health_bar(self, surface):
-        # dessiner le jauge
+        # dessiner la jauge
         pygame.draw.rect(surface,(60, 63, 60), [self.rect.x + 10, self.rect.y - 20, self.max_health, 5])
         pygame.draw.rect(surface, (111, 210, 46), [self.rect.x + 10, self.rect.y - 20, self.health, 5])
         
-
-
-
     def forward(self):
-        # le deplacement ne se fera que s'il n'y a pas de collision avec un "groupe de joueur"
+        # le déplacement ne se fera que s'il n'y a pas de collision avec un "groupe de joueur"
          if not self.jeu.check_collision(self, self.jeu.all_players):
             self.rect.x -= self.speed
-        # verifier si le joueur est en collision avec le monstre
+        # vérifier si le joueur est en collision avec le monstre
          else:
-            #  infliger des degats au joueur
+            #  infliger des dégats au joueur
             self.jeu.player.damage(self.attack)
